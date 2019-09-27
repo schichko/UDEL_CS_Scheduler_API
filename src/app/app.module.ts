257177c1901e3ab6109b292bf3b4729db5352994
Will Walker <wwalker@udel.edu>
2019-09-27 15:29:35 -0400

    working components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TermsComponent } from './terms/terms.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TermsComponent
   ],
   imports: [
     BrowserModule
