22a61c3d1e75ac9ecd7917f5a516e1bd3d3cfab3
MadeByAydin <muhammet@udel.edu>
2019-09-29 17:43:09 -0400

    attempt to add API methods

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 var mongo = require('mongoose');
+const port = 3000;
 
 /*
     Connecting to the mongodb cluster, please dont hack me.
@@ -19,21 +20,21 @@ var db = mongo.connect("mongodb+srv://muhammet:test123@cluster0-dg6n3.mongodb.ne
     Express makes it easy to handle HTTP requests for our API that will be
     used by our client-side angular code.
 */
-// var app = express();
-// app.use(bodyParser());
-// app.use(bodyParser.json({limit: '5mb'}));
-// app.use(bodyParser.urlencoded({extended:true}));
+ var app = express();
+ app.use(bodyParser.json());
+ app.use(bodyParser.urlencoded({extended:true}));
 
-// app.use(function(req, res, next) {
-//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-//     res.setHeader('Access-Control-Allow-Credentials', true);
-// });
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    //res.setHeader('Access-Control-Allow-Credentials', true);
+});
 
 var Schema = mongo.Schema;
 
 var commentsSchema = new Schema({
+    objectId: mongo.Schema.Types.ObjectId,
     name:  String,
     email: String,
     movie_id: mongo.Schema.Types.ObjectId,
@@ -43,14 +44,25 @@ var commentsSchema = new Schema({
 
 var Comment = mongo.model('comments', commentsSchema, 'comments');
 
-var angryComment = new Comment({name: 'test', 
-                                email: 'test@amazon.com',
-                                movie_id: null,
-                                text: 'this movie sucked, it wouldnt even load!'});
-
-
-angryComment.save(function (err) {
-    if (err) return handleError(err);
-    console.log("saved i guess");
-    // saved!
+var angryComment = new Comment(
+    {name: 'test user', 
+    email: 'test@amazon.com',
+    movie_id: null,
+    text: 'this movie sucked, it wouldnt even load!'
     });
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+
+app.get('/api/comments', function(req, res){
+    Comment.find({"name":"test"}, function(err, data){
+        if(err){
+            res.send(err);
+        }
+
+        res.send(data);
+        console.log(data);
+    });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
