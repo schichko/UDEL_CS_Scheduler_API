a0852baf690bc1c042ec8d6089d993ffbb2edcf7
Jake Schichko <schichko@localhost.localdomain>
2019-10-01 22:37:06 -0400

    Fixing CSS, removing practice components, adding real components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,14 @@
 import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AboutComponent } from './about/about.component'
 import { HomeComponent } from './home/home.component'
-import { PrivacyComponent} from './privacy/privacy.component'
 import { TermsComponent } from './terms/terms.component'
+import { PlanComponent } from './plan/plan.component'
 
  
 const routes: Routes = [
-  { path: 'about', component: AboutComponent},
   { path: 'term', component: TermsComponent},
   { path: 'home', component:  HomeComponent},
-  { path: 'privacy', component : PrivacyComponent},
+  { path : 'plan', component : PlanComponent},
   { path: '', redirectTo:'/home',pathMatch: 'full' }
 ];
 
